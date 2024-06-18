7d89117ec729d80d1aa175b34be08757380974cf
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-06-18 13:54:25 +0545

    api call using axios, design using scss for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import { useState } from "react";
+import "./App.scss";
+import react from "react";
+//Update Imports: Replace Switch with Routes and update your route definitions accordingly.
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import Home from "./components/Home/Home";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <h1>Hello world</h1>
+      <div className="App">
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              {/* Replace component with element */}
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/movie/:imdbID" element={<MovieDetail />}></Route>
+              <Route path="*" element={<PageNotFound />}></Route>
+            </Routes>
+          </div>
+
+          <Footer />
+        </Router>
       </div>
     </>
   );
---
+++ src/common/apis/movieApiKey.jsx
@@ -0,0 +1,1 @@
+export const ApiKey = "b8036408";
